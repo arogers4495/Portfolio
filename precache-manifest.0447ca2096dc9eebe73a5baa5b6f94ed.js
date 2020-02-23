f26f63a0ecbb08851cdfba79240e37908a0c0cad
Auston Rogers <austonrogers217@gmail.com>
2020-02-23 15:59:31 -0700

    Updates

---
+++ precache-manifest.0447ca2096dc9eebe73a5baa5b6f94ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d557b65ab0e59299ad5d5b60efe4a8a2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "625ec6d191ab8697cf18",
+    "url": "/Portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/Portfolio/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Portfolio/static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625ec6d191ab8697cf18",
+    "url": "/Portfolio/static/js/main.654d5e48.chunk.js"
+  },
+  {
+    "revision": "38e9353468597077cd3e",
+    "url": "/Portfolio/static/js/runtime-main.ed271e06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.359115b27c0a2f2a326d3ec9c0c94384.js"
+  "/Portfolio/precache-manifest.0447ca2096dc9eebe73a5baa5b6f94ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
